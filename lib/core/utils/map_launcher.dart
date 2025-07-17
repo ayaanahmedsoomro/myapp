f4f99a102696bec84f848449e5a3d5e0ac585da6
ayaan8114iaaa <ayaan8114iaaa@gmail.com>
2025-07-17 07:28:36 +0000

    url updated

--- lib/core/utils/map_launcher.dart
+++ lib/core/utils/map_launcher.dart
@@ -1,21 +1,27 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchMap(
-  String source,
-  String destination, {
-  required bool navigate,
+ String source,
+ String destination, {
+ required bool navigate,
 }) async {
-  final url =
-      'https://www.google.com/maps/dir/?api=1&origin=$source&destination=$destination&travelmode=driving${navigate ? '&navigate=yes' : ''}';
-  final uri = Uri.parse(url);
+ final Uri uri;
+
+ if (navigate) {
+    // Use navigation intent
+    uri = Uri.parse('google.navigation:q=$destination');
+  } else {
+    // Use directions with origin and destination
+    uri = Uri.parse(
+      'https://www.google.com/maps/dir/?api=1&origin=$source&destination=$destination&travelmode=driving',
+    );
+  }
 
   try {
-    // Try external app (Maps)
+    // Try launching in external map app
     if (await launchUrl(uri, mode: LaunchMode.externalNonBrowserApplication)) {
       print("Launched via external app");
-    }
-    // If fails, fallback to browser
-    else if (await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+    } else if (await launchUrl(uri, mode: LaunchMode.externalApplication)) {
       print("Launched in browser");
     } else {
       print("Could not launch");
